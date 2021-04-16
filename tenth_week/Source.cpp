f9cb1d5a5efd5a6a1454766d60643a9015670b78
Sergey <shesergey2001@gmail.com>
2021-04-16 11:24:49 +1000

    20%

---
+++ tenth_week/Source.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+class User
+{
+public:
+	virtual void printAllMarks() = 0;
+};
+
+class Teacher
+{
+	string name;
+public:
+	Teacher(string name) 
+	{
+		this->name = name;
+	}
+
+};
+
+class Admin
+{
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+
+
+
+
+
+
+
+	return 0;
+}
